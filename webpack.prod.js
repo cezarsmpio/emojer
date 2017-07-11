a24c70d98d7cbeff8d710c938c677b17282d8bd6
Cezar Luiz <cezarluiz.c@gmail.com>
2017-07-11 13:52:15 -0300

    Implemented two version of emojer, the compiled and the compiled minified.

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,7 +1,15 @@
+const path = require('path');
 const config = require('./webpack.dev');
 const webpack = require('webpack');
 
 module.exports = Object.assign({}, config, {
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/dist/',
+    filename: 'emojer.min.js',
+    library: 'emojer',
+    libraryTarget: 'umd'
+  },
   plugins: [
     new webpack.optimize.UglifyJsPlugin()
   ]
